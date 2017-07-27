50ae4696b13b56bb998e717b9b009565e3730311
Nam Nguyen <gdbtek@yahoo.com>
2017-07-26 17:13:58 -0700

    update lib

--- libraries/aws.bash
+++ libraries/aws.bash
@@ -8,7 +8,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/util.bash"
 
 function getAllowedRegions()
 {
-    echo 'ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 eu-central-1 eu-west-1 sa-east-1 us-east-1 us-west-1 us-west-2'
+    echo 'ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-west-1 eu-west-2 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2'
 }
 
 function isValidRegion()
